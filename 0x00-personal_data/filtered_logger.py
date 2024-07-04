6a510948adaa98e67023375b19ac720d6f1d1bc8
Arsany199 <arsanykamal30@gmail.com>
2024-07-04 11:04:51 +0000

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -23,12 +23,12 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """function to return filtered values using filter_datum"""
-        record.message = super().format(record)
-        f_msg = filter_datum(self.fields, self.REDACTION, record.message, self.SEPARATOR)
-        return (f_msg)
+        """funcrion returns filtered values from log record"""
+        my_rec = super().format(record)
+        return filter_datum(self.fields, self.REDACTION,
+                            my_rec, self.SEPARATOR)
