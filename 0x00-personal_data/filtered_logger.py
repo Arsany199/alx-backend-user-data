39c454bf43aac3b05eaec40814282fd0ffa00bee
Arsany199 <arsanykamal30@gmail.com>
2024-07-04 07:58:37 +0000

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""
-Module for handling Personal Data
-"""
+"""module use regex for handling Personal Data"""
 from typing import List
 import re
 import logging
@@ -9,13 +7,10 @@ from os import environ
 import mysql.connector
 
 
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
-
-
 def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
-    """ Returns a log message obfuscated """
+                 message: str, separator: str):
+    """function return a log message obfuscated"""
     for f in fields:
         message = re.sub(f'{f}=.*?{separator}',
                          f'{f}={redaction}{separator}', message)
-    return message
+    return (message)
