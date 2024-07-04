a50138cb21d2da764a64eba9acf1bb0eed13d40a
Arsany199 <arsanykamal30@gmail.com>
2024-07-04 07:56:05 +0000

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
-""" Use of regex in replacing occurrences of certain field values """
-import re
+"""
+Module for handling Personal Data
+"""
 from typing import List
+import re
+import logging
+from os import environ
+import mysql.connector
 
 
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str) -> str:
-    """ Returns regex obfuscated log messages """
-    for field in fields:
-        message = re.sub(f'{field}=(.*?){separator}',
-                         f'{field}={redaction}{separator}', message)
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """ Returns a log message obfuscated """
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
     return message
