64353dd5ed4daff01c4b94c09c5a70f851099200
Arsany199 <arsanykamal30@gmail.com>
2024-07-09 14:52:18 +0000

    my_auth

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized/', strict_slashes=False)
-def unauthorized() -> None:
+@app_views.route('/unauth/', strict_slashes=False)
+def unauth() -> None:
     """Return: Unauthorized error"""
     abort(401)
