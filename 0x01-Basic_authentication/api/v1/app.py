8ec6b27d6b420d8189a3092717741af15ed40e25
Arsany199 <arsanykamal30@gmail.com>
2024-07-09 15:05:15 +0000

    my_auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -55,11 +55,11 @@ def before_request() -> str:
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None:
-        abort(401)
-
     if auth.current_user(request) is None:
         abort(403)
+
+    if auth.authorization_header(request) is None:
+        abort(401)
 
 
 if __name__ == "__main__":
