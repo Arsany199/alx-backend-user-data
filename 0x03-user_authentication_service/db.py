894c100484f677003fe5b0fa227d541e13b25f7e
Arsany199 <arsanykamal30@gmail.com>
2024-07-17 09:06:55 +0000

    db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -2,14 +2,17 @@
 """DB module"""
 import logging
 from typing import Dict
+
 from sqlalchemy import create_engine
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
-from sqlalchemy import or_
+
 from user import Base, User
+
+logging.disable(logging.WARNING)
 
 
 class DB:
@@ -34,14 +37,19 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """function that adds a using with email and password"""
+        """add new user to the database with an email and password"""
         the_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(the_user)
-        self._session.commit()
+        try:
+            self._session.add(the_user)
+            self._session.commit()
+        except Exception as err:
+            print("Error adding user to database: {}".format(err))
+            self._session.rollback()
+            raise
         return (the_user)
 
-    def find_user_by(self, **kwargs) -> User:
-        """function to find users by a keyword (id, email,...)"""
+    def find_user_by(self, **kwargs: Dict[str, str]) -> User:
+        """find  users by a specifice attribute"""
         session = self._session
         try:
             user = session.query(User).filter_by(**kwargs).one()
@@ -52,14 +60,18 @@ class DB:
         return (user)
 
     def update_user(self, user_id: int, **kwargs) -> None:
-        """function use find_user_by to locate user then update it"""
-        user = self.find_user_by(id=user_id)
-
-        for k, _ in kwargs.items():
-            if not hasattr(user, k):
-                raise ValueError("invalid argument {}".format(k))
+        """updates a user attribute using id"""
+        try:
+            user = self.find_user_by(id=user_id)
+        except NoResultFound:
+            raise ValueError("User with id {} not found".format(user_id))
 
         for k, v in kwargs.items():
+            if not hasattr(user, k):
+                raise ValueError("User has no attribute {}".format(k))
             setattr(user, k, v)
 
-        self._session.commit()
+        try:
+            self._session.commit()
+        except InvalidRequestError:
+            raise ValueError("Invalid request")
