bb0cc2daaa8510e1591f2798dc9d944d9ef17cff
Arsany199 <arsanykamal30@gmail.com>
2024-07-17 11:10:00 +0000

    app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """model to my app file"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 AUTH = Auth()
@@ -13,7 +13,7 @@ def index():
     return jsonify({"message": "Bienvenue"})
 
 
-@app.route("/users", methods=["POST"], strict_slashes=False)
+@app.route("/users", methods=["POST"])
 def users() -> str:
     """POST user and return a josn has many informations"""
     email = request.form.get("email")
@@ -24,6 +24,25 @@ def users() -> str:
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"])
+def login() -> str:
+    """create a new session for the user store it the session ID
+    as a cookie with key "session_id" on the response
+    and return a JSON payload of the form"""
+    try:
+        email = request.form["email"]
+        password = request.form["password"]
+    except KeyError:
+        abort(400)
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    session_id = AUTH.create_session(email)
+
+    message = {"email": email, "message": "logged in"}
+    jsonify(message).set_cookie("session_id", session_id)
+    return jsonify(message)
 
 
 if __name__ == "__main__":
