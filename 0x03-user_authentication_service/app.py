483b889796d25c5d7dad7399ace0007569e7c4ef
Arsany199 <arsanykamal30@gmail.com>
2024-07-17 12:13:03 +0000

    app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -50,19 +50,16 @@ def login() -> str:
 def logout() -> str:
     """find user with session id if exist delete it and redirect to GET /
     if not found respond with 403 http"""
-    session_id = request.cookies.get("session_id", None)
-
-    if session_id is None:
-        abort(403)
-
+    session_id = request.cookies.get("session_id")
+    # Retrieve the user associated with the session ID
     user = AUTH.get_user_from_session_id(session_id)
-
+    # If no user is found, abort the request with a 403 Forbidden error
     if user is None:
         abort(403)
-
+    # Destroy the session associated with the user
     AUTH.destroy_session(user.id)
-
-    return redirect('/')
+    # Redirect to the home route
+    return redirect("/")
 
 
 if __name__ == "__main__":
